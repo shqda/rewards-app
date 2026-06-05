0047f7c248c3cb498d583c208f75ef843c7e024d
shqda <artemf642@gmail.com>
2026-06-05 12:06:47 +0300

    feat: add view for profile endpoint

--- rewards/views.py
+++ rewards/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView, Response
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = ProfileSerializer(request.user)
+        return Response(serializer.data)
