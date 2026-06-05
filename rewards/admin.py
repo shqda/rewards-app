cb8f92a986871bfc40d1adaf6d11f356fd2fb90a
shqda <artemf642@gmail.com>
2026-06-05 15:48:42 +0300

    feat: register reward models in admin

--- rewards/admin.py
+++ rewards/admin.py
@@ -7,3 +7,13 @@ from .models import RewardLog, ScheduledReward, User
 class RewardsUserAdmin(UserAdmin):
     list_display = ["username", "email", "coins", "is_staff"]
     fieldsets = UserAdmin.fieldsets + (("Rewards", {"fields": ("coins",)}),)  # type: ignore
+
+
+@admin.register(ScheduledReward)
+class ScheduledRewardAdmin(admin.ModelAdmin):
+    list_display = ["user", "amount", "execute_at"]
+
+
+@admin.register(RewardLog)
+class RewardLogAdmin(admin.ModelAdmin):
+    list_display = ["user", "amount", "given_at"]
