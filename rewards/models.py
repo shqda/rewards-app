57354d975c59ec6a4774ed4caa50d01e079632e4
shqda <artemf642@gmail.com>
2026-06-05 15:30:10 +0300

    feat: add reward models

--- rewards/models.py
+++ rewards/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     coins = models.IntegerField(default=0)
+
+
+class ScheduledReward(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    execute_at = models.DateTimeField()
+
+
+class RewardLog(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    given_at = models.DateTimeField(auto_now_add=True)
