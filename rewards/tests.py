a5186c2316073f4a1bbbe5d8bd8aa5cf751b4630
shqda <artemf642@gmail.com>
2026-06-05 15:52:44 +0300

    test: add /rewards/ endpoint test cases

--- rewards/tests.py
+++ rewards/tests.py
@@ -21,3 +21,30 @@ class ProfileEndpointTest(APITestCase):
         response = self.client.get("/api/profile/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["username"], "Vasya")
+
+class RewardsEndpointTest(APITestCase):
+    def test_requires_auth(self):
+        response = self.client.get("/api/rewards/")
+        self.assertEqual(response.status_code, 401)
+
+    def test_empty_returns_no_data(self):
+        user = User.objects.create_user(username="Vasya")
+        self.client.force_authenticate(user=user)
+        response = self.client.get("/api/rewards/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, [])
+
+    def test_returns_only_own_rewards(self):
+        vasya = User.objects.create_user(username="Vasya")
+        petya = User.objects.create_user(username="Petya")
+        RewardLog.objects.create(user=vasya, amount=33)
+        RewardLog.objects.create(user=petya, amount=44)
+        self.client.force_authenticate(user=vasya)
+        response = self.client.get("/api/rewards/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["amount"], 33)
+
+
+        
+        
